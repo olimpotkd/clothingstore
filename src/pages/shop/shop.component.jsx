97ad654659a2952c25df24d13603f1242cfbdf8b
Carlos Reynoso <olimpotkd@gmail.com>
2020-09-03 17:14:27 -0400

    Moved directory and shopt data into their own reducer. Created their selectors, and updated components with redux

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,23 +1,15 @@
-import React, { Component } from 'react';
-import SHOP_DATA from './shop.data.js';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component.jsx';
+import React from 'react';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
+import { Route } from 'react-router-dom';
 
-class ShopPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { 
-      collections: SHOP_DATA
-     }
-  }
-  render() { 
-    return (<div className="shop-page">
-      {
-        this.state.collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps}/>
-        ))
-      }
-    </div>);
-  }
-}
- 
+const ShopPage = ({ match }) => (
+  <div className="shop-page">
+    <Route exact component={CollectionsOverview} path={`${match.path}`}/>
+    <Route exact path={`${match.path}/:collectionId`} component={CollectionPage} />
+  </div>
+);
+  
+
+
 export default ShopPage;
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,23 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+//to use local storage vvvv
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
+import shopReducer from './shop/shop.reducer';
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root', //starting where in the reducer
+  storage,
+  whitelist: ['cart'] //this is the reducers we want to store, not user because it's handled by firebase
+}
+const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
-})
+  cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer
+});
+
+export default persistReducer(persistConfig, rootReducer);
