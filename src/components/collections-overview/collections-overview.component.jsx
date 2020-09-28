59a57c1474ec73094368d9337fded930a301f28d
Carlos Reynoso <olimpotkd@gmail.com>
2020-09-28 16:42:43 -0400

    removed redux-logger from production build

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -9,10 +9,10 @@ import './collections-overview.styles.scss';
 const CollectionsOverview = ({ collections }) => (
   <div className="collections-overview">
     {
-          collections.map(({ id, ...otherCollectionProps }) => (
+      collections.map(({ id, ...otherCollectionProps }) => (
         <CollectionPreview key={id} {...otherCollectionProps}/>
       ))
-          }
+    }
   </div>
 );
  
